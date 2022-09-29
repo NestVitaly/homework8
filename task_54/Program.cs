34c3fe05d534b38a62c79b5240f49aff42dda3ad
NVitaly <whiskalik@gmail.com>
2022-09-29 17:37:00 +0300

    Задача 54

---
+++ task_54/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.WriteLine("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Созданный массив: ");
+PrintArrayD(numbers);
+Console.WriteLine("Отсортированный массив: ");
+sorting(numbers);
+PrintArray(numbers);
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j != arr.GetLength(1) - 1)
+                Console.Write($"|{arr[i, j]}|, ");
+            else if (i == arr.GetLength(0) - 1 && j == arr.GetLength(1) - 1)
+                Console.WriteLine($"|{arr[i, j]}|");
+            else if (j == arr.GetLength(1) - 1)
+                Console.WriteLine($"|{arr[i, j]}|,");
+        }
+    }
+}
+
+void PrintArrayD(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j != arr.GetLength(1) - 1)
+                Console.Write($"|{arr[i, j]}|, ");
+            else if (i == arr.GetLength(0) - 1 && j == arr.GetLength(1) - 1)
+                Console.WriteLine($"|{arr[i, j]}|");
+            else if (j == arr.GetLength(1) - 1)
+                Console.WriteLine($"|{arr[i, j]}|,");
+        }
+    }
+}
+
+void sorting(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+        {
+            for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+            {
+                if (numbers[i, z] < numbers[i, z + 1])
+                {
+                    int temp = 0;
+                    temp = numbers[i, z];
+                    numbers[i, z] = numbers[i, z + 1];
+                    numbers[i, z + 1] = temp;
+                }
+            }
+        }
+    }
+}
